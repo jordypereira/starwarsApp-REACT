82ebf07f30be9e0e08c510e41b23e267617aac47
perjor <jordy.pereira@hotmail.com>
2018-03-11 15:21:31 +0100

    properties are now in a list

--- src/components/Swapi/Planet.js
+++ src/components/Swapi/Planet.js
@@ -10,6 +10,10 @@ export default class Planet extends React.Component {
       super(props);
       this.state = {
           people: [],
+          properties: [
+              'climate',
+              'terrain'
+          ]
       };
   }
   
@@ -25,9 +29,22 @@ export default class Planet extends React.Component {
           ));
   }
 
+  renderProperties() {
+      return this.state.properties.map((element, i) => (
+          <PlanetProperty name={this.capitalizeFirstLetter(element)} value={this.props.planet[element]} key={i} />
+      ));
+  }
+
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   render () {
-    const planet = this.props.planet; 
-    const residents = planet.residents.length ? this.renderPeople() : (<li className="list-group-item"> This planet has no residents</li>) ;  
+    const planet = this.props.planet;
+    const residents = planet.residents.length ? this.renderPeople() : (<li className="list-group-item"> This planet has no residents</li>) ;
+    const properties = this.state.properties.length ? this.renderProperties() : '' ;
+
+
     return (
       <div className="card" style={{width: '18rem', marginBottom: '10px'}}>
         <Link to={`/${planet.id}`}>
@@ -36,9 +53,10 @@ export default class Planet extends React.Component {
           </div>
         </Link>
         <ul className="list-group">
-          <PlanetProperty name="Climate" value={planet.climate} />
-          <PlanetProperty name="Terrain" value={planet.terrain} />
-          <PlanetProperty name="Population" value={planet.population} />
+          {properties}
+          {/*<PlanetProperty name="Climate" value={planet.climate} />*/}
+          {/*<PlanetProperty name="Terrain" value={planet.terrain} />*/}
+          {/*<PlanetProperty name="Population" value={planet.population} />*/}
           <div className="card-header">
             Residents:
           </div>
--- src/components/Swapi/PlanetProperty.js
+++ src/components/Swapi/PlanetProperty.js
@@ -1,9 +1,28 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
-const PlanetProperty = (props) => (
-  <li className="list-group-item">{props.name}: {props.value}</li>  
-);
+class PlanetProperty extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        hide: false,
+    };
+  }
+
+  onDelete () {
+    this.setState({ hide: true })
+  }
+
+  render() {
+    if(!this.state.hide){
+      return (
+        <li className="list-group-item">{this.props.name}: {this.props.value}<span className="float-right"><button className="btn btn-danger btn-sm" onClick={() => this.onDelete()}>Delete</button></span></li>  
+      );
+    }else{
+      return '';
+    }
+  }
+}
 
 PlanetProperty.propTypes = {
   name: PropTypes.string,
